fc2b34c678464ea61e206793fcd2622cd8af1d69
CaryLorrk <carylorrk@gmail.com>
2018-05-07 13:50:10 +0800

    compress: partial_sort

--- tensorflow/core/kernels/woops_tf_compress_transmit_buffer.h
+++ tensorflow/core/kernels/woops_tf_compress_transmit_buffer.h
@@ -16,24 +16,26 @@ public:
         std::map<Hostid, Bytes> ret;
         std::vector<ParamIndex> index(this->data_.size());
         std::iota(index.begin(), index.end(), 0);
-        std::sort(index.begin(), index.end(), [this](const T& lhs, const T& rhs) {
+        auto&& middle = std::next(index.begin(),
+                (this->data_.size() + COMPRESSION_RATIO - 1) / COMPRESSION_RATIO);
+        std::partial_sort(index.begin(), middle, index.end(), [this](const T& lhs, const T& rhs) {
             auto&& data = this->data_;
             return std::abs(data[lhs]) > std::abs(data[rhs]);
         });
         
-        index.resize((this->data_.size() + COMPRESSION_RATIO - 1) / COMPRESSION_RATIO);
-        std::sort(index.begin(), index.end());
+        std::sort(index.begin(), middle);
 
         auto&& kv = partitions.begin();
-        for (auto&& idx : index) {
+        for (auto it = index.begin(); it != middle; ++it) {
+            const ParamIndex& idx = *it;
             T& val = this->data_[idx];
+
             while (idx >= kv->second.end) ++kv;
             Hostid server = kv->first;
             ret[server].append((Byte*)&(idx), (Byte*)(&(idx) + 1));
             ret[server].append((Byte*)&(val), (Byte*)(&(val) + 1));
             val = 0;
         }
-
         return ret;
     }
 };
